f20f1fcf1b4e89df3339180425f047a47acf4970
Renzify <168178279+Renzify@users.noreply.github.com>
2026-03-05 11:00:19 +0800

    add event & template routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,17 @@ import path from "path";
 import cors from "cors";
 
 import { ENV } from "./lib/env.ts";
+import { templateRouter } from "./routes/template.route.ts";
+import { eventRouter } from "./routes/event.route.ts";
 
 const app = express();
 const { PORT, CLIENT_URL, NODE_ENV } = ENV;
 
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
+
+app.use("/api/templates", templateRouter);
+app.use("/api/events", eventRouter);
 
 if (NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
