533d5f023db9eca0e3211c2e9f19ce9e6c73dc96
ej-bgnt <ethanbognott@gmail.com>
2026-03-10 15:59:50 +0800

    Modify app jsx and event details jsx

--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -129,18 +129,3 @@ export default function EventDetails() {
     </div>
   );
 }
-
-
-
-function EventDetails() {
-  return (
-    <div>
-      <div className="flex flex-col items-center ">
-        <div className="w-7/8 border border-black">
-          <h1 className="text-xl font-semibold">Event Details</h1>
-        </div>
-      </div>
-    </div>
-  );
-}
-
