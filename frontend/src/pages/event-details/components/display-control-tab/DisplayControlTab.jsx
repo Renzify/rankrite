0613b43ec5614575b392855cfe6229849ec72168
Manuel <barcelonamanuelmarc@gmail.com>
2026-03-16 14:30:09 +0800

    refactor: refactor contestant list of display tab

--- frontend/src/pages/event-details/components/display-control-tab/DisplayControlTab.jsx
+++ frontend/src/pages/event-details/components/display-control-tab/DisplayControlTab.jsx
@@ -5,6 +5,7 @@ import useDisplayControlEffects from "./hooks/useDisplayControlEffects";
 import useDisplayControlHandlers from "./hooks/useDisplayControlHandlers";
 import LivePreview from "./components/LivePreview";
 import ViewingControls from "./components/ViewingControls";
+import ContestantList from "./components/ContestantList";
 
 export default function DisplayControlTab() {
   const {
@@ -152,70 +153,7 @@ export default function DisplayControlTab() {
       </div>
 
       <div className="app-table-wrap">
-        <table className="table table-zebra">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Contestant</th>
-              <th>Delegation</th>
-              <th>Preview State</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contestants.length ? (
-              contestants.map((contestant, index) => {
-                const isLive = safeActiveIndex === index;
-                const isNext = nextIndex === index;
-
-                return (
-                  <tr key={contestant.id}>
-                    <th>{index + 1}</th>
-                    <td
-                      className="max-w-[11rem] truncate sm:max-w-[16rem]"
-                      title={contestant.fullName}
-                    >
-                      {contestant.fullName}
-                    </td>
-                    <td
-                      className="max-w-[10rem] truncate sm:max-w-[14rem]"
-                      title={contestant.delegation}
-                    >
-                      {contestant.delegation}
-                    </td>
-                    <td>
-                      <span
-                        className={`badge ${
-                          isLive
-                            ? isBlackout
-                              ? "badge-error"
-                              : "badge-success"
-                            : isNext
-                              ? "badge-info"
-                              : "badge-ghost"
-                        }`}
-                      >
-                        {isLive
-                          ? isBlackout
-                            ? "Hidden"
-                            : "Live"
-                          : isNext
-                            ? "Next"
-                            : "Queued"}
-                      </span>
-                    </td>
-                  </tr>
-                );
-              })
-            ) : (
-              <tr>
-                <td colSpan={4} className="text-base-content/60">
-                  No contestants available. Import contestants in Contestants
-                  tab first.
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+        <ContestantList contestants={contestants} />
       </div>
     </div>
   );
